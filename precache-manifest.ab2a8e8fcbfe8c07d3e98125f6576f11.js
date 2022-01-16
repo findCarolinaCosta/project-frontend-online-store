2ee9fcee680c9e7d25783932d82a424c7870881c
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-16 19:38:59 -0300

    Updates

---
+++ precache-manifest.ab2a8e8fcbfe8c07d3e98125f6576f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2169e79fa59d979e3419ab2c7c3f08f2",
+    "url": "/findCarolinaCosta/project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "0d6305e421f4d0a8d510",
+    "url": "/findCarolinaCosta/project-frontend-online-store/static/css/main.dde38cbb.chunk.css"
+  },
+  {
+    "revision": "1434dac87e2298689709",
+    "url": "/findCarolinaCosta/project-frontend-online-store/static/js/2.2d862f4f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/findCarolinaCosta/project-frontend-online-store/static/js/2.2d862f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6305e421f4d0a8d510",
+    "url": "/findCarolinaCosta/project-frontend-online-store/static/js/main.82f499b4.chunk.js"
+  },
+  {
+    "revision": "078ccb879803c39af00e",
+    "url": "/findCarolinaCosta/project-frontend-online-store/static/js/runtime-main.6b1052bb.js"
+  }
+]);
